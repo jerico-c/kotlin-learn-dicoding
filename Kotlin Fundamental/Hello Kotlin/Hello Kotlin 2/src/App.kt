6797ac7bde88b3b76b54c5fa73c38b3d97c60481
Jerico Christianto <jericochristianto@gmail.com>
2025-08-02 19:38:27 +0700

    initial

---
+++ Kotlin Fundamental/Hello Kotlin/Hello Kotlin 2/src/App.kt
@@ -0,0 +1,13 @@
+// main function
+fun main() {
+    val name = "Ahmad Abu Hasan"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
+}
+/*
+   output:
+       Hello my name is Ahmad Abu Hasan
+       Always true
+*/
